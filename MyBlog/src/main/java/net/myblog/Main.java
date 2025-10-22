39fd02c94a2152ae2db426a16ab2f290bf123207
jinguumirai <jingunmisaki@gmail.com>
2025-10-23 03:27:50 +0900

    Create a simple java project.

---
+++ MyBlog/src/main/java/net/myblog/Main.java
@@ -0,0 +1,10 @@
+package net.myblog;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(Main.class, args);
+    }
+}
