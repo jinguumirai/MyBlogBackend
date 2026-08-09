b9c91178872993cf9b58f2659ae1d2d6336eccba
jinguumirai <jingunmisaki@gmail.com>
2026-08-10 03:08:04 +0900

    remove idea setting and debug Jenkinsfile

--- Login/src/main/java/net/myblog/login/config/WebSecurityConfig.java
+++ Login/src/main/java/net/myblog/login/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authz ->
                 authz.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/identify/login/**", "/doc.html", "/webjars/&zwnj;**", "/v2/api-docs",
                         "/swagger-resources/**&zwnj;", "identify/api-docs/**", "identify/v3/api-docs/**",
